ea11af57c950c19a15aa8eff97632a8a5f029863
pushme <1141846557@qq.com>
2025-03-18 22:29:41 +0800

    fix bugs

--- src/core/userapi.js
+++ src/core/userapi.js
@@ -15,7 +15,7 @@ export default function userapi(socket, userAliasMap, socketMap, dataSql) {
 		// 注册事件处理中
 		try {
 			// 校验逻辑
-			if (name.length > 20 || !/^[\u4e00-\u9fa5\w-]+$/.test(name)) {
+			if (name.length > 20 || !/^[\w-]+$/.test(name)) {
 				socket.emit("register", {
 					error: "用户名包含非法字符（仅支持中文、字母、数字、下划线和连字符）",
 				});
