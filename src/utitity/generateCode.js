af711933a70d62d1e620e9ff5b09859b2da450e9
pushme <1141846557@qq.com>
2025-03-18 20:07:19 +0800

    修改验证码长度

--- src/utitity/generateCode.js
+++ src/utitity/generateCode.js
@@ -1,9 +1,9 @@
 /**
  * 生成指定长度的数字验证码
- * @param {number} length - 验证码长度，默认5位
+ * @param {number} length - 验证码长度，默认4位
  * @returns {string} 数字验证码字符串
  */
-export function generateNumericCode(length = 5) {
+export function generateNumericCode(length = 4) {
 	const min = Math.pow(10, length - 1);
 	const max = Math.pow(10, length) - 1;
 	return Math.floor(min + Math.random() * (max - min + 1)).toString();
